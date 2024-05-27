43e927fe6126684baedda6704c28ee66293855e1
Matthew Emes <memes@matthewemes.com>
2024-05-27 13:26:15 -0700

    docs: Bump example ahead of v3.0.1 release

--- examples/quick-start/main.tf
+++ examples/quick-start/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 module "bastion" {
   source                = "memes/private-bastion/google"
-  version               = "3.0.0"
+  version               = "3.0.1"
   proxy_container_image = var.proxy_container_image
   name                  = var.name
   project_id            = var.project_id
